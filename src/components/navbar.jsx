f8ef4d91e56f65795c90bd2081211d70e0047e23
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2024-06-22 19:42:56 +0000

    navbar init

---
+++ src/components/navbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import logo from '../logo.svg';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+
+export default function MyNavbar() {
+    return (
+        <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
+            <Container>
+                <Navbar.Brand href="#home">
+                    <img
+                        src={logo}
+                        width="30"
+                        height="30"
+                        className="d-inline-block align-top"
+                        alt="Logo"
+                    />{' '}
+                    MyWebsite
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ml-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#about">About</Nav.Link>
+                        <Nav.Link href="#services">Services</Nav.Link>
+                        <Nav.Link href="#contact">Contact</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+}
