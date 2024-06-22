56a6d94bb9930000258449f0ca15f303e097bad5
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2024-06-22 19:55:15 +0000

    navbar links name update

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -19,10 +19,10 @@ export default function MyNavbar() {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#about">About</Nav.Link>
-                        <Nav.Link href="#services">Services</Nav.Link>
-                        <Nav.Link href="#contact">Contact</Nav.Link>
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="about">About</Nav.Link>
+                        <Nav.Link href="services">Services</Nav.Link>
+                        <Nav.Link href="contact">Contact</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
