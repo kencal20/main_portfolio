dc8528ed0971240c67f07e815952940bfc589130
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2024-06-22 19:43:15 +0000

    homeScreen update

--- src/screens/homeScreen.jsx
+++ src/screens/homeScreen.jsx
@@ -1,7 +1,48 @@
 import React from 'react'
-
+import logo from '../logo.svg'
+import Navbar from '../components/navbar'
+import { Button } from 'react-bootstrap'
 export default function HomeScreen() {
-  return (
-    <div>homeScreen</div>
-  )
+    return (
+        <>
+            <Navbar />
+            <div style={styles.container}>
+                <div>
+                    <img src={logo} style={styles.logo} alt="logo" />
+                </div>
+                <div style={styles.content}>
+                    <h1 style={styles.h1}>About Me</h1>
+                    <hr />
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum sit similique quaerat aperiam? Esse, incidunt.
+                    </p>
+                    <Button>Read more</Button>
+                </div>
+            </div>
+        </>
+    )
+}
+
+const styles = {
+    container: {
+        display: 'flex',
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#f5f5f5',
+        minWidth: '100vw',
+        minHeight: '100vh',
+    },
+    logo: {
+        height: '10em',
+        width: '10em',
+        margin: '3em',
+        padding: '1em',
+        borderRadius: '50%',
+        border: '1px solid #000',
+    },
+    content: {
+        margin: '3em',
+    }
 }
