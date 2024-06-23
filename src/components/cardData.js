49c69f7fa15a616357a146c4d70d3683199e13a4
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2024-06-23 18:30:04 +0000

    data for card

---
+++ src/components/cardData.js
@@ -0,0 +1,25 @@
+import profile from '../img/doc3.jpeg';
+
+export const cardData = [
+  {
+    title: 'Todo List Mobile App',
+    imgSrc: profile,
+    imgAlt: 'Todo List Mobile App',
+    link: '#',
+    children: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi,',
+  },
+  {
+    title: 'Todo List Web App',
+    imgSrc: profile,
+    imgAlt: 'Todo List Web App',
+    link: '#',
+    children: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi,',
+  },
+  {
+    title: 'Todo List Web App',
+    imgSrc: profile,
+    imgAlt: 'Todo List Web App',
+    link: '#',
+    children: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi,',
+  },
+];
