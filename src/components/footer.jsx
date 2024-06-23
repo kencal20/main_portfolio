206de5bb1fca469ad43b3a8f8a0a8d4c95492702
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2024-06-23 22:34:50 +0000

    update date in footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,10 +3,11 @@ import { Link } from 'react-router-dom';
 import styles from '../css/footer.module.css';
 
 export default function FooterComponent() {
+    const year = new Date().getFullYear()
     return (
         <footer className={styles.footer}>
             <div className={styles.footerContent}>
-                <p className={styles.footerText}>&copy; 2024 Kenneth Adjei-Mensah. All rights reserved.</p>
+                <p className={styles.footerText}>&copy; {year} Kenneth Adjei-Mensah. All rights reserved.</p>
                 <nav className={styles.footerNav}>
                     <Link to="#" className={styles.footerLink}>Terms of Service</Link>
                     <Link to="#" className={styles.footerLink}>Privacy</Link>
