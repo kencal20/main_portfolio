862d1923c27e4a5a81296248782f041d95aabfb4
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2024-06-23 13:04:54 +0000

    card update

--- src/components/cardThumbnail.jsx
+++ src/components/cardThumbnail.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
+import React from 'react';
 
-export default function CardThumbnail({ imgSrc,imgAlt,link, title, children }) {
-    return (
-        <div class="card" style={{ width: "18rem" }}>
-            <img src={imgSrc} class="card-img-top" alt={imgAlt} />
-            <div class="card-body">
-                <h5 class="card-title">{title}</h5>
-                <p class="card-text">
-                    {children}
-                </p>
-                <a href={link} class="btn btn-primary">Go somewhere</a>
-            </div>
-        </div>
-    )
+export default function CardThumbnail({ imgSrc, imgAlt, link, title, children, styles }) {
+  return (
+    <div className="card" style={{ width: '18rem', ...styles }}>
+      <img src={imgSrc} className="card-img-top" alt={imgAlt} />
+      <div className="card-body">
+        <h5 className="card-title">{title}</h5>
+        <p className="card-text">{children}</p>
+        <a href={link} className="btn btn-primary">
+          Go somewhere
+        </a>
+      </div>
+    </div>
+  );
 }
