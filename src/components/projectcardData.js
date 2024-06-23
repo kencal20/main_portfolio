3d760e25a6d344ed2a0506d14d7dc599d6d27a06
Kenneth Adjei-mensah Adjetey <kenadjei20@gmail.com>
2024-06-23 22:13:25 +0000

    cardData name update

--- src/components/projectcardData.js
+++ src/components/projectcardData.js
@@ -5,14 +5,14 @@ export const cardData = [
     title: 'Todo List Mobile App',
     imgSrc: profile,
     imgAlt: 'Todo List Mobile App',
-    link: '#',
+    link: 'https://github.com/kencal20/todoApp/',
     children: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi,',
   },
   {
     title: 'Todo List Web App',
     imgSrc: profile,
     imgAlt: 'Todo List Web App',
-    link: '#',
+    link: 'https://github.com/kencal20/todolist_fullstack',
     children: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi,',
   },
   {
